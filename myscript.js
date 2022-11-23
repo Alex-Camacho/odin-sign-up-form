573ed3faa2cf6fd5fb378d5cf03eab8a0225889a
Alex-Camacho <cuentascriptico@gmail.com>
2022-11-23 23:25:40 +0100

    JS password validation working

--- myscript.js
+++ myscript.js
@@ -1,2 +1,14 @@
 let password = document.getElementById("password");
-let confirmpassword = document.getElementById("confirmpassword");
+let confirmPassword = document.getElementById("confirmpassword");
+let alert = document.getElementById("alert");
+
+function checkPassword () {
+    alert.innerText = password.value == confirmPassword.value ? 
+    "" : "Passwords don't match"
+}
+
+password.addEventListener("keyup", () => {
+    if (confirmPassword.value.length != 0) checkPassword();
+})
+
+confirmPassword.addEventListener("keyup", checkPassword);
